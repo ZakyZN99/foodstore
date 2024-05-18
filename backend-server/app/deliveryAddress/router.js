6927bf299a2af2b505cf81d2018363c3460c1b62
ZakyZN <zakyzn1999@gmail.com>
2024-05-18 13:26:58 +0800

    new

--- backend-server/app/deliveryAddress/router.js
+++ backend-server/app/deliveryAddress/router.js
@@ -5,7 +5,6 @@ const router = require('express').Router();
 router.get('/delivery-address', police_check('view', 'DeliveryAddress'), deliveryAddressController.index);
 router.post('/delivery-address', police_check('create', 'DeliveryAddress'), deliveryAddressController.store);
 router.put('/delivery-address/:id', police_check('update', 'DeliveryAddress'), deliveryAddressController.update);
-router.get('/delivery-address/:id', police_check('view', 'DeliveryAddress'), deliveryAddressController.findById);
 router.delete('/delivery-address/:id', police_check('delete', 'DeliveryAddress'), deliveryAddressController.destroy);
 
 //dari tutorial
